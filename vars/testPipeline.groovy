38d5f7673e03eeeff6d57cec91d554c79fe683b1
Cesar Daniel Perez <cesdperez@gmail.com>
2019-07-19 13:15:03 +0200

    fixed testPipeline

---
+++ vars/testPipeline.groovy
@@ -0,0 +1,22 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def params = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = params
+    body()
+
+    pipeline {
+        agent any
+        stages {
+            stage('stage 1') {
+                sh "echo This is stage 1 for application ${params.application}"
+            }
+            stage('stage 2') {
+                sh "echo This is stage 2 for application ${params.application}"
+            }
+            stage('stage 3') {
+                sh "echo This is stage 3 for application ${params.application}"
+            }
+        }
+    }
+}
